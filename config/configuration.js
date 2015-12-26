5fe3a87ba599870ee663b1075088205f0ca2d5d2
Suryadeep Pal <suryadeep10@gmail.com>
2015-12-26 13:22:04 +0530

    added support for session handling

--- config/configuration.js
+++ config/configuration.js
@@ -1,14 +1,34 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var session = require('express-session')
+const MongoStore = require('connect-mongo')(session);
 
 module.exports = function (app) {
+
+    //bodyparser for parsing post request bodies
     app.use( bodyParser.json() );         // to support JSON-encoded bodies
     app.use( bodyParser.urlencoded({      // to support URL-encoded bodies
         extended: true
     }));
 
+    //setting the cookie parser and session
+    app.use(cookieParser());
+    app.use(session({
+        secret: 'PsafkSKD34ahufAjqqwd23ranwkefjw5rfar11rWEF',
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+    }));
+
+    //setting up the view engine
+    app.set('views', './views');
+    app.set('view engine', 'jade');
+
+    //DB connection
     app.set('db', 'mongodb://localhost/incubation')
 
+    //Connecting the DB
     mongoose.connect(app.get('db'));
 
     var db = mongoose.connection;
--- routes/registration/login.js
+++ routes/registration/login.js
@@ -1,6 +1,23 @@
 module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.render('index');
+    });
+
+    app.get('/logout', function (req, res) {
+        req.session.destroy(function (err) {
+            if (err) {
+                console.log(err);
+            }else {
+                res.send('You have been logged out');
+            }
+        })
+    })
+
     app.get('/dashboard', function (req, res) {
-        res.send('Hey there!');
+        console.log(req.session);
+        // res.cookie('email', 'suryadeep10@gmail.com', { maxAge: 900000, httpOnly: true });
+        // console.log(req.session);
+        res.render('dashboard');
     });
 
     app.post('/login', function (req, res) {
@@ -16,6 +33,7 @@ module.exports = function (app) {
                     res.sendStatus(400);
                 } else {
                     if (studentData.password == credential.password) {
+                        req.session.email = studentData.email;
                         res.sendStatus(200);
                     } else {
                         res.sendStatus(401);
